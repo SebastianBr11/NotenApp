0df559cf9e86498748f1d33b7c97d2f4c946bacc
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-04-02 16:01:33 +0200

    feat: :sparkles: make state persistent

---
+++ storage/state.ts
@@ -0,0 +1,21 @@
+import {
+	configureObservablePersistence,
+	persistObservable,
+} from '@legendapp/state/persist'
+import { ObservablePersistAsyncStorage } from '@legendapp/state/persist-plugins/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { schools } from './grades'
+
+// Global configuration
+configureObservablePersistence({
+	// Use AsyncStorage in React Native
+	pluginLocal: ObservablePersistAsyncStorage,
+	localOptions: {
+		asyncStorage: {
+			// The AsyncStorage plugin needs to be given the implementation of AsyncStorage
+			AsyncStorage,
+		},
+	},
+})
+
+persistObservable(schools, { local: 'schools' })

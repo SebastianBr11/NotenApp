560b482661302a5a307617330401d33f1de44c6e
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-05-22 13:06:09 +0200

    feat: allow adding grades from grade screen

--- src/app/subjects/[subject].tsx
+++ src/app/subjects/[subject].tsx
@@ -1,18 +1,31 @@
 import { observer } from '@legendapp/state/react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 
 import { t } from '@/i18n/util'
+import { useSetupBottomSheetModal } from '@/modules/overview/components/useSetupBottomSheetModal'
+import AddGradeButton from '@/modules/subject/components/AddGradeButton'
+import AddGradeForm, {
+	FormData,
+} from '@/modules/subject/components/AddGradeForm'
 import SemesterView from '@/modules/subject/components/SemesterView'
 import { schools } from '@/storage/grades'
 import { calculateAverageOfSemesters } from '@/util/gradeCalcFos'
+import { calculateGradeFromPoints } from '@/util/school'
+import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
 
 export default observer(SubjectScreen)
 
 function SubjectScreen() {
-	const { styles } = useStyles(stylesheet)
+	const { styles, theme } = useStyles(stylesheet)
+	const {
+		bottomSheetModalRef,
+		handlePresentModalPress,
+		handleSheetChanges,
+		snapPoints,
+	} = useSetupBottomSheetModal({ snapPoints: ['80%'] })
 
 	const { subject: subjectId, selectedClass } = useLocalSearchParams()
 	const classes = schools.classes.get()
@@ -23,6 +36,29 @@ function SubjectScreen() {
 	} = classes[Number(selectedClass)].subjects.find(
 		subject => subject.id === Number(subjectId),
 	)!
+
+	const handleAddGrade = (data: FormData) => {
+		console.log('adding')
+		const didSucceed = schools.addGrade(
+			Number(selectedClass),
+			Number(subjectId),
+			1,
+			{
+				grade: calculateGradeFromPoints(Number(data.points)),
+				type: data.type,
+				points: Number(data.points),
+			},
+		)
+
+		console.log(didSucceed)
+
+		if (!didSucceed) {
+			Alert.alert(
+				'Es ist ein Fehler aufgetreten!',
+				'Es gibt bereits eine Schulaufgabe in diesem Schuljahr',
+			)
+		}
+	}
 
 	return (
 		<View style={styles.container}>
@@ -42,6 +78,25 @@ function SubjectScreen() {
 				<SemesterView semesterNumber={1} semester={semesterOne} />
 				<SemesterView semesterNumber={2} semester={semesterTwo} />
 			</View>
+
+			<BottomSheetModal
+				ref={bottomSheetModalRef}
+				snapPoints={snapPoints}
+				onChange={handleSheetChanges}
+				handleIndicatorStyle={{ backgroundColor: theme.colors.text1 }}
+				handleStyle={{
+					backgroundColor: theme.colors.bg2,
+					borderTopStartRadius: 20,
+					borderTopEndRadius: 20,
+				}}
+				backgroundStyle={{ backgroundColor: theme.colors.bg2 }}
+			>
+				<BottomSheetView style={styles.bottomSheetContainer}>
+					<AddGradeForm onSubmit={handleAddGrade} />
+				</BottomSheetView>
+			</BottomSheetModal>
+
+			<AddGradeButton onPress={handlePresentModalPress} />
 		</View>
 	)
 }
@@ -85,5 +140,8 @@ const stylesheet = createStyleSheet(theme => ({
 		paddingHorizontal: theme.spacing['6xl'],
 		paddingVertical: theme.spacing['4xl'],
 		gap: theme.spacing['5xl'],
+	},
+	bottomSheetContainer: {
+		flex: 1,
 	},
 }))
--- src/util/school.ts
+++ src/util/school.ts
@@ -3,3 +3,22 @@ import { ClassType } from '@/storage/grades'
 export const calculateAmountOfSubjects = (classes: ClassType[]) => {
 	return classes.reduce((acc, cur) => acc + cur.subjects.length, 0)
 }
+
+// TODO: Add tests
+export const calculateGradeFromPoints = (points: number) => {
+	if (points === 0) {
+		return 6
+	} else if (points <= 3) {
+		return 5
+	} else if (points <= 6) {
+		return 4
+	} else if (points <= 9) {
+		return 3
+	} else if (points <= 12) {
+		return 2
+	} else if (points <= 15) {
+		return 1
+	}
+
+	return 6
+}

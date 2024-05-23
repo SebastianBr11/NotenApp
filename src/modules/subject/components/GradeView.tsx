38d0476288908292437e9afe261c02d5cf6c9f1d
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-05-23 17:49:50 +0200

    feat: :sparkles: create modal for editing existing grades

--- src/modules/subject/components/GradeView.tsx
+++ src/modules/subject/components/GradeView.tsx
@@ -1,31 +1,69 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, PressableStateCallbackType, Text, View } from 'react-native'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 
+import TextInputLabel from '@/components/form/TextInputLabel'
 import { ifDarkElse } from '@/constants/themes'
+import { useSetupBottomSheetModal } from '@/hooks/useSetupBottomSheetModal'
 import { SingleGradeType } from '@/storage/grades'
+import { Feather } from '@expo/vector-icons'
+import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
 
 type GradeViewType = {
 	singleGrade: SingleGradeType
 }
 
 export default function GradeView({ singleGrade }: GradeViewType) {
-	const { styles } = useStyles(stylesheet)
+	const { styles, theme } = useStyles(stylesheet)
+
+	const { bottomSheetModalRef, handlePresentModalPress, handleSheetChanges } =
+		useSetupBottomSheetModal()
+
+	const handlePress = () => {
+		handlePresentModalPress()
+	}
 
 	return (
-		<View style={styles.container}>
-			<Text style={styles.gradeTypeText}>{singleGrade.type}</Text>
-			<Text style={styles.pointsText}>{singleGrade.points}</Text>
-		</View>
+		<>
+			<Pressable onPress={handlePress} style={styles.container}>
+				<View style={styles.typeWrapper}>
+					<Text style={styles.gradeTypeText}>{singleGrade.type}</Text>
+					<Feather
+						name='edit'
+						size={theme.spacing['2xl']}
+						color={theme.colors.text5}
+					/>
+				</View>
+				<Text style={styles.pointsText}>{singleGrade.points}</Text>
+			</Pressable>
+			<BottomSheetModal
+				ref={bottomSheetModalRef}
+				enableDynamicSizing
+				onChange={handleSheetChanges}
+				handleIndicatorStyle={{ backgroundColor: theme.colors.text1 }}
+				handleStyle={{
+					backgroundColor: theme.colors.bg2,
+					borderTopStartRadius: 20,
+					borderTopEndRadius: 20,
+				}}
+				backgroundStyle={{ backgroundColor: theme.colors.bg2 }}
+			>
+				<BottomSheetView>
+					<Text>hi</Text>
+					<TextInputLabel>Points</TextInputLabel>
+					{/* <TextInput value={singleGrade.points} /> */}
+				</BottomSheetView>
+			</BottomSheetModal>
+		</>
 	)
 }
 
 const stylesheet = createStyleSheet(theme => ({
-	container: {
+	container: ({ pressed }: PressableStateCallbackType) => ({
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'space-between',
-		backgroundColor: theme.colors.bg2,
+		backgroundColor: pressed ? theme.colors.bg3 : theme.colors.bg2,
 		paddingVertical: theme.spacing['3xl'],
 		paddingHorizontal: 2 * theme.spacing['3xl'],
 		borderRadius: theme.spacing['4xl'],
@@ -35,6 +73,11 @@ const stylesheet = createStyleSheet(theme => ({
 			theme.colors.main[300],
 		),
 		borderWidth: 1,
+	}),
+	typeWrapper: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: theme.spacing.xl,
 	},
 	gradeTypeText: {
 		fontSize: theme.fontSizes['xl'],

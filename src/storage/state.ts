632f937b52a94a92b71815c5736920b0b1d5aba7
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-07-02 16:45:23 +0200

    chore: disable state persistence in dev
    The persistence sometimes caused issues with state not being initialized correctly in dev

--- src/storage/state.ts
+++ src/storage/state.ts
@@ -1,17 +1,21 @@
+import { IS_DEV } from '@/constants/isDev'
 import { ObservablePersistMMKV } from '@legendapp/state/persist-plugins/mmkv'
 import { configureObservableSync, syncObservable } from '@legendapp/state/sync'
 import grades from './grades'
 
-// Global configuration
-configureObservableSync({
-	// Use react-native-mmkv in React Native
-	persist: {
-		plugin: ObservablePersistMMKV,
-	},
-})
+// Only enable state persistence in development
+if (!IS_DEV) {
+	// Global configuration
+	configureObservableSync({
+		// Use react-native-mmkv in React Native
+		persist: {
+			plugin: ObservablePersistMMKV,
+		},
+	})
 
-syncObservable(grades, {
-	persist: {
-		name: 'grades',
-	},
-})
+	syncObservable(grades, {
+		persist: {
+			name: 'grades',
+		},
+	})
+}

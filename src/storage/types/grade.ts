e7c6d85b3487163e8808b2f204b644bb4db09805
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-08-14 18:10:42 +0200

    refactor: make grade type not differentiate points and corresponding grades
    This should make it easier to work with the type
    
    reverts 4057aa3

--- src/storage/types/grade.ts
+++ src/storage/types/grade.ts
@@ -1,26 +1,5 @@
-export type Grade = { id: string } & (
-	| {
-			grade: 1
-			points: 15 | 14 | 13
-	  }
-	| {
-			grade: 2
-			points: 12 | 11 | 10
-	  }
-	| {
-			grade: 3
-			points: 9 | 8 | 7
-	  }
-	| {
-			grade: 4
-			points: 6 | 5 | 4
-	  }
-	| {
-			grade: 5
-			points: 3 | 2 | 1
-	  }
-	| {
-			grade: 6
-			points: 0
-	  }
-)
+export type Grade = {
+	id: string
+	grade: 1 | 2 | 3 | 4 | 5 | 6
+	points: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15
+}

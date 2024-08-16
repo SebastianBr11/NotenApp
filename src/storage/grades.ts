SebastianBr11/NotenApp

commit 25b87d1715c4faa0ee2c386f71d96ed62dc4f0a9

refactor: move grade deletion and updating logic to main state location

src/storage/grades.ts
@@ -106,6 +106,29 @@ function findSubject(subjectId: string) {
 	return lastUsedClass$.value.subjects.find(s => s.id.get() === subjectId)
 }
 
+function updateGrade(
+	subjectId: string,
+	semesterNumber: 1 | 2,
+	gradeId: string,
+	gradePartial: Partial<Pick<SingleGradeType, 'grade' | 'points'>>,
+) {
+	// @ts-expect-error
+	return findGrade(subjectId, semesterNumber, gradeId)?.set(
+		(grade$: SingleGradeType) => ({
+			...grade$,
+			gradePartial,
+		}),
+	)
+}
+
+function deleteGrade(
+	subjectId: string,
+	semesterNumber: 1 | 2,
+	gradeId: string,
+) {
+	findGrade(subjectId, semesterNumber, gradeId)?.delete()
+}
+
 const amountOfSubjects$ = observable(() =>
 	calculateAmountOfSubjects(classes$.get()),
 )
@@ -122,6 +145,8 @@ const grades$ = observable({
 	addGrade,
 	findGrade,
 	findSubject,
+	updateGrade,
+	deleteGrade,
 })
 
 export default grades$

src/modules/subject/components/SubjectGrades.tsx
@@ -54,22 +54,13 @@ export default function SubjectGrades({ subject }: SubjectGradesProps) {
 	}
 
 	const handleChangeGradePoints = (newPoints: number) => {
-		const grade$ = grades$.findGrade(
-			subject.id,
-			selectedGrade.semester,
-			selectedGrade.id,
-		)
-		grade$?.points.set(newPoints)
+		grades$.updateGrade(subject.id, selectedGrade.semester, selectedGrade.id, {
+			points: newPoints as SingleGradeType['points'],
+		})
 	}
 
 	const handleDeleteGrade = () => {
-		const grade$ = grades$.findGrade(
-			subject.id,
-			selectedGrade.semester,
-			selectedGrade.id,
-		)
-		grade$?.delete()
-
+		grades$.deleteGrade(subject.id, selectedGrade.semester, selectedGrade.id)
 		bottomSheetModalRef.current?.dismiss()
 	}
 

commit 103cdf404c77bdb3c20980c5e2ed5b4a372899fd

fix: updating grades not working
Fixes regression caused by 25b87d1

src/storage/grades.ts
@@ -114,11 +114,25 @@ function updateGrade(
 ) {
 	// @ts-expect-error
 	return findGrade(subjectId, semesterNumber, gradeId)?.set(
-		(grade$: SingleGradeType) => ({
-			...grade$,
-			gradePartial,
-		}),
+		(grade: SingleGradeType) => {
+			console.log(grade, gradePartial)
+			console.log({ ...grade, ...gradePartial })
+			return {
+				...grade,
+				...gradePartial,
+			}
+		},
 	)
+}
+
+function updateSubject(
+	subjectId: string,
+	subjectPartial: Partial<SubjectType>,
+) {
+	return findSubject(subjectId)?.set((subject$: SubjectType) => ({
+		...subject$,
+		subjectPartial,
+	}))
 }
 
 function deleteGrade(
@@ -127,6 +141,10 @@ function deleteGrade(
 	gradeId: string,
 ) {
 	findGrade(subjectId, semesterNumber, gradeId)?.delete()
+}
+
+function deleteSubject(subjectId: string) {
+	findSubject(subjectId)?.delete()
 }
 
 const amountOfSubjects$ = observable(() =>
@@ -146,7 +164,9 @@ const grades$ = observable({
 	findGrade,
 	findSubject,
 	updateGrade,
+	updateSubject,
 	deleteGrade,
+	deleteSubject,
 })
 
 export default grades$